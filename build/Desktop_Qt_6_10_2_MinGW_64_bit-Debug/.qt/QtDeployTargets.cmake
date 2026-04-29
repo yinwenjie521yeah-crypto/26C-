7834bd5dd34184794f89e9727078a1dda2934453
3612048127@qq.com <3612048127@qq.com>
2026-04-29 16:55:23 +0800

    remove build files from repository

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_final_FILE C:/Users/Yin/Desktop/final/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/final.exe)
-set(__QT_DEPLOY_TARGET_final_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_final_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
